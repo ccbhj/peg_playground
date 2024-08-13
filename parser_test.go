e7619008455d9c53891a640a1aa934a4d461d108
Bingjia Chen <bingjia.chen@seamoney.com>
2024-08-13 13:32:40 +0800

    fix(parser_test.go): add some more tests for the syntax

--- parser_test.go
+++ parser_test.go
@@ -14,3 +14,52 @@ func TestPrintTree(t *testing.T) {
 	}
 	println("-----------------------------------------------------")
 }
+
+func TestValue(t *testing.T) {
+	var cases = []struct {
+		Desc string
+		Expr string
+	}{
+		{
+			Desc: "NIL",
+			Expr: "nil",
+		},
+		{
+			Desc: "boolean",
+			Expr: "#t",
+		},
+		{
+			Desc: "decimal integer",
+			Expr: "123456",
+		},
+		{
+			Desc: "hex integer",
+			Expr: "0x1f1",
+		},
+		{
+			Desc: "short string",
+			Expr: `"hello\n\x1f\u1234\U12345678"`,
+		},
+		{
+			Desc: "long string",
+			Expr: `"""foobar"""`,
+		},
+		{
+			Desc: "identifier",
+			Expr: `"""hello\n\x1f\u1234\U12345678"""`,
+		},
+		{
+			Desc: "expression",
+			Expr: `(HELLO world 123 "!!!")`,
+		},
+	}
+
+	for _, c := range cases {
+		println("[TEST] " + c.Desc)
+
+		if err := PrintAST(c.Expr); err != nil {
+			t.Fatal(err)
+		}
+		println("-----------------------------------------------------")
+	}
+}
